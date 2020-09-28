d29190652bcc03dc52eb65377efe01fa6bb54b36
Saulo Joab <37988252+saulojoab@users.noreply.github.com>
2020-09-27 21:03:14 -0300

    converting user page to react hooks

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaHome,FaLaptop, FaSplotch } from 'react-icons/fa'
 
 import { Link } from 'react-router-dom'
@@ -12,75 +12,73 @@ import ValueBox from '../../components/ValueBox'
 
 import Info from './styles'
 
-export default class User extends Component {
+export default function User (props) {
+    const [user, setUser] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    state = {
-        user: [],
-        loading: 1
-    }
-
-    async componentDidMount() {
-        const { match } = this.props
+    async function getUser() {
+        const { match } = props;
         const usuarioNome = decodeURIComponent(match.params.user)
 
         const nome = await api.get(`/users/${usuarioNome}`)
 
-        this.setState({ user: nome.data, loading: 0 })
+        setUser(nome.data);
+        setLoading(false);
 
-        console.log(this.state.user)
+        console.log(user)
     }
 
-    render(){
+    useEffect(() => {
+        getUser()
+    }, [])
+    
 
-        const { user, loading } = this.state
-
-        if (loading) {
-            return <Loading>Carregando... *u*</Loading>
-        }
-
-        return (
-            <Container>
-                <Owner>
-                    <Link to="/">Página inicial</Link>
-                    <h1>{user.name}</h1>
-                    <img src={user.avatar_url} alt={user.login}/>
-                    <p>{user.bio}</p>
-                    <Info>
-                        <li>
-                            <FaLaptop />
-                            { user.blog ? <strong>{user.blog}</strong> : 'No website'}
-                        </li>
-
-                        <li>
-                            <FaSplotch />
-                            { user.company ? <strong>{user.company}</strong> : 'No Company'}
-                        </li>
-
-                        <li>
-                            <FaHome />
-                            { user.location ? <strong>{user.location}</strong> : 'No Location'}
-                        </li>
-                    </Info>
-                    <section className="valueBox-list">
-                        <ValueBox bgColor="#F3AB93">
-                            <span><Link to={`/usuario/${encodeURIComponent(user.login)}/seguidores`}>Seguidores: </Link></span>
-                            <strong>{user.followers}</strong>
-                        </ValueBox>
-                        <ValueBox bgColor="#B3A9C4">
-                            <span><Link to={`/usuario/${encodeURIComponent(user.login)}/seguindo`}>Seguindo: </Link></span>
-                            <strong>{user.following}</strong>
-                        </ValueBox>
-                        <ValueBox bgColor="#8FC4C4">
-                        <span><Link to={`/usuario/${encodeURIComponent(user.login)}/gists`}>Gists: </Link></span>
-                            <strong>{user.public_gists}</strong>
-                        </ValueBox>
-                        <ValueBox bgColor="#DAC">
-                            <span><Link to={`/usuario/${encodeURIComponent(user.login)}/repositorios`}>Repositorios: </Link></span>
-                            <strong>{user.public_repos}</strong>
-                        </ValueBox>
-                    </section>
-                </Owner>
-            </Container>
-        )
+    if (loading) {
+        return <Loading>Carregando... *u*</Loading>
     }
+
+    return (
+        <Container>
+            <Owner>
+                <Link to="/">Página inicial</Link>
+                <h1>{user.name}</h1>
+                <img src={user.avatar_url} alt={user.login}/>
+                <p>{user.bio}</p>
+                <Info>
+                    <li>
+                        <FaLaptop />
+                        { user.blog ? <strong>{user.blog}</strong> : 'No website'}
+                    </li>
+
+                    <li>
+                        <FaSplotch />
+                        { user.company ? <strong>{user.company}</strong> : 'No Company'}
+                    </li>
+
+                    <li>
+                        <FaHome />
+                        { user.location ? <strong>{user.location}</strong> : 'No Location'}
+                    </li>
+                </Info>
+                <section className="valueBox-list">
+                    <ValueBox bgColor="#F3AB93">
+                        <span><Link to={`/usuario/${encodeURIComponent(user.login)}/seguidores`}>Seguidores: </Link></span>
+                        <strong>{user.followers}</strong>
+                    </ValueBox>
+                    <ValueBox bgColor="#B3A9C4">
+                        <span><Link to={`/usuario/${encodeURIComponent(user.login)}/seguindo`}>Seguindo: </Link></span>
+                        <strong>{user.following}</strong>
+                    </ValueBox>
+                    <ValueBox bgColor="#8FC4C4">
+                    <span><Link to={`/usuario/${encodeURIComponent(user.login)}/gists`}>Gists: </Link></span>
+                        <strong>{user.public_gists}</strong>
+                    </ValueBox>
+                    <ValueBox bgColor="#DAC">
+                        <span><Link to={`/usuario/${encodeURIComponent(user.login)}/repositorios`}>Repositorios: </Link></span>
+                        <strong>{user.public_repos}</strong>
+                    </ValueBox>
+                </section>
+            </Owner>
+        </Container>
+    )
 }
